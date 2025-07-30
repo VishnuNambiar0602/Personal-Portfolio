c256d6533b981f57d5e52109aded3114190548a9
Vishnu Nambiar <vishnunambiar2006@gmail.com>
2025-07-30 16:03:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
